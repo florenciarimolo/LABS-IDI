b309f7756799b72974816ca8f88f23867dab1184
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2017-10-23 22:16:19 +0200

    ex5 sessio3 fet

--- bloc1/sessio3/ex5/MyGLWidget.cpp
+++ bloc1/sessio3/ex5/MyGLWidget.cpp
@@ -4,157 +4,154 @@
 
 MyGLWidget::MyGLWidget (QWidget* parent) : QOpenGLWidget(parent)
 {
-  setFocusPolicy(Qt::ClickFocus);  // per rebre events de teclat
+    setFocusPolicy(Qt::ClickFocus);  // per rebre events de teclat
 }
 
 MyGLWidget::~MyGLWidget ()
 {
-  if (program != NULL)
-    delete program;
+    if (program != NULL)
+        delete program;
 }
 
 void MyGLWidget::initializeGL ()
 {
-  // Cal inicialitzar l'ús de les funcions d'OpenGL
-  initializeOpenGLFunctions();
+    // Cal inicialitzar l'ús de les funcions d'OpenGL
+    initializeOpenGLFunctions();
     ex = ey = ez = 1.0;
-  tx = ty = tz = 1.0;
-  angle = (M_PI/4);
-  glClearColor (0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
-  carregaShaders();
-  createBuffers();
+    tx = ty = tz = 0.0;
+    angle = 0.0;
+    glClearColor (0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
+    carregaShaders();
+    createBuffers();
     modelTransform();
 }
 
 void MyGLWidget::paintGL ()
 {
-  glClear (GL_COLOR_BUFFER_BIT);  // Esborrem el frame-buffer
-
-  // Activem l'Array a pintar 
-  glBindVertexArray(VAO);
- 
-  // Pintem l'escena
+    glClear (GL_COLOR_BUFFER_BIT);  // Esborrem el frame-buffer
+    
+    // Activem l'Array a pintar
+    glBindVertexArray(VAO);
+    
+    // Pintem l'escena
     modelTransform();
-  glDrawArrays(GL_TRIANGLES, 0, 3);
-  
-  // Desactivem el VAO
-  glBindVertexArray(0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+    // Desactivem el VAO
+    glBindVertexArray(0);
 }
 
 void MyGLWidget::resizeGL (int w, int h)
 {
-  glViewport (0, 0, w, h);
+    glViewport (0, 0, w, h);
 }
 
 void MyGLWidget::createBuffers ()
 {
-  glm::vec3 Vertices[3];  // Tres vèrtexs amb X, Y i Z
-  Vertices[0] = glm::vec3(-1.0, -1.0, 0.0);
-  Vertices[1] = glm::vec3(1.0, -1.0, 0.0);
-  Vertices[2] = glm::vec3(0.0, 1.0, 0.0);
-
-  glm::vec3 Colors[3];  // Tres vèrtexs amb X, Y i Z
-  Colors[0] = glm::vec3(1.0, 0.0, 0.0);
-  Colors[1] = glm::vec3(0.0, 1.0, 0.0);
-  Colors[2] = glm::vec3(0.0, 0.0, 1.0);
-  
-  // Creació del Vertex Array Object (VAO) que usarem per pintar
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-
-  // Creació del buffer amb les dades dels vèrtexs
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-  // Activem l'atribut que farem servir per vèrtex	
-  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(vertexLoc);
-
-  glGenBuffers(1, &VBO_color);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(colorLoc);
-
-  // Desactivem el VAO
-  glBindVertexArray(0);
+    glm::vec3 Vertices[3];  // Tres vèrtexs amb X, Y i Z
+    Vertices[0] = glm::vec3(-1.0, -1.0, 0.0);
+    Vertices[1] = glm::vec3(1.0, -1.0, 0.0);
+    Vertices[2] = glm::vec3(0.0, 1.0, 0.0);
+    
+    glm::vec3 Colors[3];  //
+    Colors[0] = glm::vec3(1.0, 0.0, 0.0);
+    Colors[1] = glm::vec3(0.0, 1.0, 0.0);
+    Colors[2] = glm::vec3(0.0, 0.0, 1.0);
+    
+    // Creació del Vertex Array Object (VAO) que usarem per pintar
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    // Creació del buffer amb les dades dels vèrtexs
+    glGenBuffers(1, &VBO_vert);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_vert);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    // Activem l'atribut que farem servir per vèrtex
+    glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(vertexLoc);
+    
+    glGenBuffers(1, &VBO_color);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(colorLoc);
+    
+    // Desactivem el VAO
+    glBindVertexArray(0);
 }
 
 void MyGLWidget::carregaShaders()
 {
-  // Creem els shaders per al fragment shader i el vertex shader
-  QOpenGLShader fs (QOpenGLShader::Fragment, this);
-  QOpenGLShader vs (QOpenGLShader::Vertex, this);
-  // Carreguem el codi dels fitxers i els compilem
-  fs.compileSourceFile("/Users/florenciarimolo/Dropbox/Informatica/idi/lab/bloc1/sessio3/ex5/shaders/fragshad.frag");
-  vs.compileSourceFile("/Users/florenciarimolo/Dropbox/Informatica/idi/lab/bloc1/sessio3/ex5/shaders/vertshad.vert");
-  // Creem el program
-  program = new QOpenGLShaderProgram(this);
-  // Li afegim els shaders corresponents
-  program->addShader(&fs);
-  program->addShader(&vs);
-  // Linkem el program
-  program->link();
-  // Indiquem que aquest és el program que volem usar
-  program->bind();
-
-  // Obtenim identificador per a l'atribut “vertex” del vertex shader
-  vertexLoc = glGetAttribLocation (program->programId(), "vertex");
-
-  colorLoc = glGetAttribLocation (program->programId(), "color");
-
-
+    // Creem els shaders per al fragment shader i el vertex shader
+    QOpenGLShader fs (QOpenGLShader::Fragment, this);
+    QOpenGLShader vs (QOpenGLShader::Vertex, this);
+    // Carreguem el codi dels fitxers i els compilem
+    fs.compileSourceFile("/Users/florenciarimolo/Dropbox/uni/idi/lab/bloc1/sessio3/ex5/shaders/fragshad.frag");
+    vs.compileSourceFile("/Users/florenciarimolo/Dropbox/uni/idi/lab/bloc1/sessio3/ex5/shaders/vertshad.vert");
+    // Creem el program
+    program = new QOpenGLShaderProgram(this);
+    // Li afegim els shaders corresponents
+    program->addShader(&fs);
+    program->addShader(&vs);
+    // Linkem el program
+    program->link();
+    // Indiquem que aquest és el program que volem usar
+    program->bind();
     
-  transLoc = glGetUniformLocation (program->programId(), "TG");
+    // Obtenim identificador per a l'atribut “vertex” del vertex shader
+    vertexLoc = glGetAttribLocation (program->programId(), "vertex");
+    
+    colorLoc = glGetAttribLocation (program->programId(), "color");
+    
+    transLoc = glGetUniformLocation (program->programId(), "TG");
 }
 
 void MyGLWidget::modelTransform () {
     glm::mat4 TG (1.0); // Matriu de transformació, inicialment identitat
-    
+    TG = glm::rotate (TG, angle, glm::vec3 (0., 0., 1.));
     TG = glm::translate (TG, glm::vec3 (tx, ty, tz));
     TG = glm::scale(TG, glm::vec3(ex, ey, ez));
-    TG = glm::rotate (TG, angle, glm::vec3 (0., 0., 1.));
     glUniformMatrix4fv (transLoc, 1, GL_FALSE, &TG[0][0]);
 }
 
 void MyGLWidget::keyPressEvent (QKeyEvent *e)
 {
-  makeCurrent();
-  switch ( e->key() )  {
-    case Qt::Key_S :
-       ex += 0.1;
-       break;
-    case Qt::Key_D :
-      ex -= 0.1;
-          break;
-  case Qt::Key_E :
-          ey += 0.5;
-          break;
-  case Qt::Key_X :
-          ey -= 0.5;
-          break;
-   case Qt::Key_Up :
-          ty += 0.1;
-          angle += M_PI/4;
-          break;
-      case Qt::Key_Down :
-          ty -= 0.1;
-          angle += M_PI/4;
-          break;
-      case Qt::Key_Left :
-          tx -= 0.1;
-          angle += M_PI/4;
-          break;
-      case Qt::Key_Right :
-          tx += 0.1;
-          angle += M_PI/4;
-          break;
-
-    default:
-      e->ignore (); // propagar al pare
-  }
-  update();
+    makeCurrent();
+    switch ( e->key() )  {
+        case Qt::Key_S :
+            ex += 0.1;
+            break;
+        case Qt::Key_D :
+            ex -= 0.1;
+            break;
+        case Qt::Key_E :
+            ey += 0.5;
+            break;
+        case Qt::Key_X :
+            ey -= 0.5;
+            break;
+        case Qt::Key_Up :
+            ty += 0.1;
+            angle += M_PI/4;
+            break;
+        case Qt::Key_Down :
+            ty -= 0.1;
+            angle += M_PI/4;
+            break;
+        case Qt::Key_Left :
+            tx -= 0.1;
+            angle += M_PI/4;
+            break;
+        case Qt::Key_Right :
+            tx += 0.1;
+            angle += M_PI/4;
+            break;
+            
+        default:
+            e->ignore (); // propagar al pare
+    }
+    update();
 }
 
