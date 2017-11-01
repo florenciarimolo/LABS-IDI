b53aa4e115618a65f6bce1880684adfc853dc527
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2017-11-01 18:19:15 +0100

    sessio3 bloc2

---
+++ bloc2/sessio3/ex3/MyGLWidget.h
@@ -0,0 +1,68 @@
+#define GLM_FORCE_RADIANS
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include <QKeyEvent>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "model.h"
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core 
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ( );
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ( );
+    // resizeGL - És cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);
+    // mouse events
+    virtual void mousePressEvent   (QMouseEvent *event);
+    virtual void mouseReleaseEvent (QMouseEvent *event);
+    virtual void mouseMoveEvent    (QMouseEvent *event);
+    // keyPressEvent - Es cridat quan es prem una tecla
+    //virtual void keyPressEvent (QKeyEvent *event);
+
+  private:
+    void createBuffers ();
+    void carregaShaders ();
+    void modelTransformPatricioA ();
+    void modelTransformPatricioB ();
+    void modelTransformTerra ();
+    void projectTransform ();
+    void viewTransform ();
+    void iniCamera ();
+    void radiEsferaContenidora ();
+    // Interaccio amb cursor
+    typedef enum {NOINTERACCIO, ROTACIO} Interaccio;
+    Interaccio interaccio;
+    int        xClick, yClick;
+    
+    Model model;
+    
+    GLuint viewLoc;
+    GLuint projectaLoc;
+    // attribute locations
+    GLuint vertexLoc, colorLoc;
+    // uniform locations
+    GLuint transLoc;
+    // VAO i VBO names
+    GLuint VAO_Terra, VBO_TerraVert, VBO_TerraColor;
+    GLuint VAO_Model, VBO_ModelVert, VBO_ModelMat;
+    // Program
+    QOpenGLShaderProgram *program;
+    // Internal vars
+    float scale;
+    glm::vec3 pos, obs, vrp, up, centre, centreBase, v, eixX, eixY, eixZ;
+    float left, right, bottom, ra, top, znear, zfar;
+    float radi, angle, theta, psi, phi, deltaA, deltaFov;
+};
+
